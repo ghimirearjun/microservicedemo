a5e5bc4c2aeb625c6ee62c1eaf148b7a0d6143ce
Arjun Ghimire <arjun.ghimire@outcodesoftware.com>
2022-09-28 13:30:22 +0545

    project setup

---
+++ PlatformService/SyncDataServices/Grpc/GrpcPlatformService.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using AutoMapper;
+using Grpc.Core;
+using PlatformService.Data;
+
+namespace PlatformService.SyncDataServices.Grpc
+{
+    public class GrpcPlatformService : GrpcPlatform.GrpcPlatformBase
+    {
+        private readonly IPlatformRepo _respository;
+        private readonly IMapper _mapper;
+
+        public GrpcPlatformService(IPlatformRepo respository, IMapper mapper)
+        {
+            _respository = respository;
+            _mapper = mapper;
+        }
+
+        public override Task<PlatformResponse> GetAllPlatforms(GetAllRequest request, ServerCallContext context)
+        {
+            var response = new PlatformResponse();
+            var platforms = _respository.GetAllPlatforms();
+
+            foreach ( var plat in platforms)
+            {
+                response.Platform.Add(_mapper.Map<GrpcPlatformModel>(plat));
+            }
+
+            return Task.FromResult(response);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# microservicedemo
+﻿# Dotnet-Microservices
+# Here
+• Building two .NET Microservices using the REST API pattern <br />
+• Working with dedicated persistence layers for both services <br />
+• Deploying our services to Kubernetes cluster <br />
+• Employing the API Gateway pattern to route to our services <br />
+• Building Synchronous messaging between services (HTTP & gRPC) <br />
+• Building Asynchronous messaging between services using an Event Bus (RabbitMQ)<br />
